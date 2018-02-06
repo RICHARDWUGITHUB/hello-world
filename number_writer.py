990f7108f6b969fe958604f3215fc147441ec903
RICHARDWUGITHUB <36183341+RICHARDWUGITHUB@users.noreply.github.com>
2018-02-06 16:25:48 +0800

    Add files via upload

---
+++ number_writer.py
@@ -0,0 +1,14 @@
+import json 
+ 
+numbers = [2, 3, 5, 7, 11, 13] 
+ 
+filename = 'numbers.json' 
+with open(filename, 'w') as f_obj: 
+    json.dump(numbers, f_obj) 
+
+filename = 'numbers.json'
+with open(filename) as f_obj:
+    numbers = json.load(f_obj)
+    #numbers = f_obj.read()
+     
+print(numbers) 
